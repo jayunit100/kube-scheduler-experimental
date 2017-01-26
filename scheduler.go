be490230ecd1eebf57f8b611563dbacb2586a9bc
jayunit100 <jay@apache.org>
2017-01-26 14:40:12 -0500

    importing of predicates works (no client dep) but not priorities

--- scheduler.go
+++ scheduler.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"k8s.io/kubernetes/plugin/pkg/scheduler/algorithm/predicates"
-	"k8s.io/kubernetes/plugin/pkg/scheduler/algorithm/priorities"
+//	"k8s.io/kubernetes/plugin/pkg/scheduler/algorithm/priorities"
 	 "github.com/golang/glog"
 )
 
 func main() {
 	_ = predicates.EBSVolumeFilter
-	_ = priorities.BalancedResourceAllocationMap
+//	_ = priorities.BalancedResourceAllocationMap
 
 	glog.Info("abcd")
 }
